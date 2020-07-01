c73972b9d3fae2ceaf4014a6b86f49409dcab809
Tomasz Sosnowski <t.sosnowski2508@wp.pl>
2020-07-01 22:32:51 +0200

    add simple frame

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import gui.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.pack();
+    }
+}
---
+++ src/main/java/gui/MainFrame.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame(){
+        super.setTitle("15 Piece Sliding Puzzle Game");
+        super.setMinimumSize(new Dimension(350,350));
+        super.setAlwaysOnTop(true);
+        super.setLocation(100,100);
+        super.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        super.setLayout(new GridLayout(4,4));
+        super.setVisible(true);
+    }
+}
