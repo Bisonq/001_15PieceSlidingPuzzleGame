53ca9c61179dd8cc67c356686c820edb1c6c67f0
Tomasz Sosnowski <t.sosnowski2508@wp.pl>
2020-07-02 11:32:11 +0200

    add Engine class - operating a puzzle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,10 @@
+import engine.Engine;
 import engine.MyPuzzel;
 import gui.MainFrame;
 
 public class Main {
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
-
-        for (int i = 0 ; i < 16 ; i ++)
-            frame.getContentPane().add(new MyPuzzel());
-
-        frame.pack();
+        Engine engine = new Engine(frame);
     }
 }
---
+++ src/main/java/engine/Engine.java
@@ -0,0 +1,47 @@
+package engine;
+
+import gui.MainFrame;
+
+import java.util.*;
+
+public class Engine{
+    private MainFrame frame;
+    private MyPuzzel[] puzzleTab;
+    private MyPuzzel blankPuzzel;
+
+    public Engine(MainFrame frame){
+        this.frame = frame;
+        this.puzzleTab = createTableOfPuzzle();
+        shufflePuzzle(this.puzzleTab);
+        addPuzzleIntoFrame(this.frame, this.puzzleTab);
+        this.blankPuzzel = this.puzzleTab[puzzleTab.length-1];
+    }
+
+    private MyPuzzel[] createTableOfPuzzle(){
+        MyPuzzel[] myPuzzelsTab = new MyPuzzel[16];
+        for(int i = 0 ; i < 16 ; i ++)
+            myPuzzelsTab[i] = new MyPuzzel();
+        return myPuzzelsTab;
+    }
+
+    private void addPuzzleIntoFrame(MainFrame mf, MyPuzzel[] tab){
+        for(int i = 0 ; i < tab.length - 1 ; i++)
+            mf.add(tab[i]);
+        MyPuzzel puzzel = tab[tab.length-1];
+        puzzel.setText("");
+        mf.add(puzzel);
+        mf.pack();
+    }
+
+    private void shufflePuzzle(MyPuzzel[] tab){
+        Random random = new Random();
+        for (int i = 0 ; i < tab.length - 1 ; i ++){
+            int pom = random.nextInt(tab.length - 1);
+            MyPuzzel var = tab[i];
+            tab[i] = tab[pom];
+            tab[pom] = var;
+        }
+    }
+
+
+}
